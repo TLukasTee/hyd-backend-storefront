df682955a151594eb95fc600303b5005029b6ae7
TLukasTNeu <ovonear@gmail.com>
2024-07-27 17:47:45 +0200

    test

--- src/modules/order/components/order-summary/index.tsx
+++ src/modules/order/components/order-summary/index.tsx
@@ -16,10 +16,10 @@ const OrderSummary = ({ order }: OrderSummaryProps) => {
 
   return (
     <div>
-      <h2 className="text-base-semi">Order Summary</h2>
+      <h2 className="text-base-semi">Bestellung</h2>
       <div className="text-small-regular text-ui-fg-base my-2">
         <div className="flex items-center justify-between text-base-regular text-ui-fg-base mb-2">
-          <span>Subtotal</span>
+          <span>Zwischensumme</span>
           <span>{getAmount(order.subtotal)}</span>
         </div>
         <div className="flex flex-col gap-y-1">
@@ -31,22 +31,22 @@ const OrderSummary = ({ order }: OrderSummaryProps) => {
           )}
           {order.gift_card_total > 0 && (
             <div className="flex items-center justify-between">
-              <span>Discount</span>
+              <span>Gutschein</span>
               <span>- {getAmount(order.gift_card_total)}</span>
             </div>
           )}
           <div className="flex items-center justify-between">
-            <span>Shipping</span>
+            <span>Versand</span>
             <span>{getAmount(order.shipping_total)}</span>
           </div>
-          <div className="flex items-center justify-between">
-            <span>Taxes</span>
+          {/* <div className="flex items-center justify-between">
+            <span>Mwst</span>
             <span>{getAmount(order.tax_total)}</span>
-          </div>
+          </div> */}
         </div>
         <div className="h-px w-full border-b border-gray-200 border-dashed my-4" />
         <div className="flex items-center justify-between text-base-regular text-ui-fg-base mb-2">
-          <span>Total</span>
+          <span>Gesamt</span>
           <span>{getAmount(order.total)}</span>
         </div>
       </div>
